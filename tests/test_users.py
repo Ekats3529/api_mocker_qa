51358d0ff67b47d79f782fd464effcf71f3fa691
Ekaterina <suhowa.ekaterina2013@yandex.ru>
2026-03-28 22:33:45 +0400

    fix+test: fix scenarios for users requests

--- tests/test_users.py
+++ tests/test_users.py
@@ -20,25 +20,14 @@ class TestUsers:
         assert isinstance(response.json()['data'], list)
 
     @allure.title("GET: Получение пользователя с {user_id}")
-    @pytest.mark.parametrize("user_id", [1, 6, 15])
+    @pytest.mark.parametrize("user_id", [2, 6, 9])
     def test_get_user_by_id(self, user_id):
         response = self.service.get_by_id(user_id)
         assert response.status_code == 200
         assert response.json()["data"]['id'] == user_id
 
-    @allure.title("POST: Создание нового пользователя")
-    def test_create_user(self):
-        payload = {
-            "name": "Ivan Ivanov",
-            "username": "ivan_test",
-            "email": "ivan@example.com"
-        }
-        response = self.service.create(payload)
-        assert response.status_code in [200, 201], f"Ожидался 200/201, получили {response.status_code}"
-        assert response.json()["data"]["name"] == payload["name"]
-
     @allure.title("PUT: Полное обновление пользователя с ID {user_id}")
-    @pytest.mark.parametrize("user_id", [1])
+    @pytest.mark.parametrize("user_id", [2])
     def test_update_user(self, user_id):
         payload = {
             "name": "Updated Name",
@@ -50,7 +39,7 @@ class TestUsers:
         assert response.json()["data"]["name"] == "Updated Name"
 
     @allure.title("PATCH: Частичное обновление (email) пользователя с ID {user_id}")
-    @pytest.mark.parametrize("user_id", [1])
+    @pytest.mark.parametrize("user_id", [2])
     def test_patch_user(self, user_id):
         payload = {"email": "patch_test@example.com"}
         response = self.service.patch(user_id, payload)
@@ -58,8 +47,7 @@ class TestUsers:
         assert response.json()["data"]["email"] == payload["email"]
 
     @allure.title("DELETE: Удаление пользователя с ID {user_id}")
-    @pytest.mark.parametrize("user_id", [1])
+    @pytest.mark.parametrize("user_id", [16])
     def test_delete_user(self, user_id):
         response = self.service.delete(user_id)
-        # DELETE обычно возвращает 200, 204 (No Content) или 202
         assert response.status_code in [200, 202, 204]
